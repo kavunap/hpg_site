5c00ec87f8c650e78a224259bc76230a402a621e
kavunap <pkavuna@gmail.com>
2020-01-22 14:16:32 +0200

    footer removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to messages_url, notice: 'Logged in!'
+      redirect_to root_url, notice: 'Logged in!'
     else
       flash.now.alert = 'Email or password is invalid'
       render :new
